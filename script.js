38e68d52608f1d81df064f1ec721a9e27b138065
Saranya Barani <62207142+Saranya-git-20@users.noreply.github.com>
2025-07-31 16:04:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+Promise.all(image.map(downloadImage))
+  .then(() => {
+    console.log('All images downloaded successfully.');
+  })
+  .catch(error => {
+    console.error('Error downloading one or more images:', error);
+  });
+function downloadImage(url){
+	 return fetch(url)
+    .then(response => {
+      if (!response.ok) throw new Error(`Failed to load ${url}`);
+      return response.blob();
+    })
+    .then(blob => {
+      const img = document.createElement('output');
+      img.src = URL.createObjectURL(blob);
+      img.alt = url.split('/').pop();
+      return img;
+    });
+}
